b51e068735518dbe66e8447426d5930a5a94089d
Jeff <jpconwi@gmail.com>
2020-08-30 22:23:25 -0700

    added model and mockdb

--- app.js
+++ app.js
@@ -1,39 +1,40 @@
 //express
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 //for terminal
-var colors = require('colors');
+var colors = require("colors");
+//to convert to json
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 //mongo
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 //hide db
-require('dotenv/config')
+require("dotenv/config");
 
 //middleware routes
-const postsRoute = require('./routes/illuminati');
+const postsRoute = require("./routes/illuminati");
 
 //illuminati route
-app.use('/illuminati', postsRoute)
-
+app.use("/illuminati", postsRoute);
 
 //home
-app.get('/', (req, res) => {
-    res.send('GET READY TO DIE')
+app.get("/", (req, res) => {
+  res.send("GET READY TO DIE");
 });
 
-
-
-
 //db
-mongoose.connect(process.env.DB_CONNECTION,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    },
-    () => console.log('connected to database'.blue.underline),
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => console.log("connected to database".blue.underline)
 );
 
-
 app.listen(port, () => {
-    console.log(`AEGIS REFLECTOR!! http://localhost:${port}`.underline.brightMagenta.bgGrey)
-})
+  console.log(
+    `AEGIS REFLECTOR!! http://localhost:${port}`.underline.brightMagenta.bgGrey
+  );
+});
