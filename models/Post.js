007d6eec180ad13051c14ef401a07c534803e09c
Jeff <jpconwi@gmail.com>
2020-09-01 12:27:55 -0700

    fixed connection to database

--- models/Post.js
+++ models/Post.js
@@ -10,4 +10,4 @@ const PostSchema = mongoose.Schema({
 })
 
 
-module.exports = mongoose.model('Posts', PostSchema);
+module.exports = mongoose.model('frameData', PostSchema);
--- routes/illuminati.js
+++ routes/illuminati.js
@@ -1,20 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Post = require('../models/Post')
+const Post = require("../models/Post");
 
-
-router.get('/', (req, res) => {
-    res.send('TREMBLE!');
-    console.log("someone is here".red)
+router.get("/", (req, res) => {
+  res.send("TREMBLE!");
+  console.log("someone is here".red);
 });
 
-router.post('/', (req, res)=>{
-    console.log(req.body);
-})
+router.post("/", (req, res) => {
+  console.log(req.body);
+  const post = new Post({
+    title: req.body.title,
+    description: req.body.description,
+  });
 
-router.get('/tackle', (req, res) => {
-    res.send('TACKLE!');
-    console.log("TACKLE HEADBUTT")
+  post
+    .save()
+    .then(data => {
+      res.json(data);
+    })
+    .catch(error => {
+      res.json({ message: error });
+    });
+});
+
+router.get("/tackle", (req, res) => {
+  res.send("TACKLE!");
+  console.log("TACKLE HEADBUTT");
 });
 
 module.exports = router;
