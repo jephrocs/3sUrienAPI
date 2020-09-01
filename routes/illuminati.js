2ffc25848c226b5119559c66fc4cedb979e52cfb
Jeff <jpconwi@gmail.com>
2020-09-01 12:57:10 -0700

    added delete, update, add post

--- routes/illuminati.js
+++ routes/illuminati.js
@@ -2,31 +2,80 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
 
-router.get("/", (req, res) => {
-  res.send("TREMBLE!");
-  console.log("someone is here".red);
+//get all posts
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+    console.log("someone is here".red);
+  } catch (error) {
+    res.json({ message: error });
+  }
 });
 
-router.post("/", (req, res) => {
+//submit post
+router.post("/", async (req, res) => {
   console.log(req.body);
   const post = new Post({
     title: req.body.title,
     description: req.body.description,
   });
-
-  post
-    .save()
-    .then(data => {
-      res.json(data);
-    })
-    .catch(error => {
-      res.json({ message: error });
-    });
+  try {
+    const savedPost = await post.save();
+    res.json(savedPost);
+  } catch (error) {
+    res.json({ message: error });
+  }
+  //   post
+  //     .save()
+  //     .then(data => {
+  //       res.json(data);
+  //     })
+  //     .catch(error => {
+  //       res.json({ message: error });
+  //     });
 });
 
 router.get("/tackle", (req, res) => {
   res.send("TACKLE!");
   console.log("TACKLE HEADBUTT");
+});
+
+//get specific post
+
+router.get("/:postId", async (req, res) => {
+  try {
+    console.log(req.params.postId);
+    const post = await Post.findById(req.params.postId);
+    res.json(post);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+//update
+
+router.patch("/:postId", async (req, res) => {
+  try {
+    console.log(req.params.postId);
+    const updatedPost = await Post.updateOne(
+      { _id: req.params.postId },
+      { $set: { title: req.body.title } }
+    );
+    res.json(updatedPost);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+//delete
+router.delete("/:postId", async (req, res) => {
+  try {
+    const removedpost = await Post.remove({ _id: req.params.postId });
+    res.json(removedpost);
+  } catch (error) {
+    res.json({ message: error });
+  }
 });
 
 module.exports = router;
